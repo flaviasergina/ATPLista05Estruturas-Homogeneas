136c7eafa073b1c8f6950b981a744e03dcdfdf18
flaviasergina <128700706+flaviasergina@users.noreply.github.com>
2023-05-30 22:04:00 -0300

    Update questao8.cs

--- questao8.cs
+++ questao8.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,26 +8,72 @@ namespace Lista5Atp
 {
     public static class questao8
     {
-        public static void rodar()
+        public static void rodar(string[] args)
         {
-            int tamVetor = 20;
-            int[] vetor = new int[tamVetor];
-            int menorValor = int.MaxValue;
-            int pos = 0;
+            int[,] matriz = new int[4, 4];
 
-            for (int i = 0; i < tamVetor; i++)
+            PreencherMatriz(matriz);
+            Console.WriteLine("Matriz preenchida:");
+            ImprimirMatriz(matriz);
+
+            int somaAbaixoDiagonal = CalcularSomaAbaixoDiagonal(matriz);
+            Console.WriteLine("Soma dos elementos abaixo da diagonal principal: " + somaAbaixoDiagonal);
+
+            Console.WriteLine("Elementos da diagonal principal:");
+            ExibirDiagonalPrincipal(matriz);
+
+            Console.ReadLine();
+        }
+
+        static void PreencherMatriz(int[,] matriz)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
             {
-                Console.WriteLine("Digite um numero para a posição " + i + " do vetor");
-                vetor[i] = int.Parse(Console.ReadLine());
-
-                if (menorValor > vetor[i])
+                for (int j = 0; j < matriz.GetLength(1); j++)
                 {
-                    menorValor = vetor[i];
-                    pos = i;
+                    matriz[i, j] = random.Next(1, 10);
+                }
+            }
+        }
+
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int CalcularSomaAbaixoDiagonal(int[,] matriz)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (j < i)
+                    {
+                        soma += matriz[i, j];
+                    }
                 }
             }
 
-            Console.WriteLine("O menor valor do vetor é: " + menorValor + " e sua posição é: " + pos);
+            return soma;
+        }
+
+        static void ExibirDiagonalPrincipal(int[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+            Console.WriteLine();
         }
     }
-}
